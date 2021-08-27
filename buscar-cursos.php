5cd2593a01e1f972282896946a09314bd6572483
Guilherme M. Pires <guilherme.pires@unicred.com.br>
2021-08-27 08:21:24 -0300

    Adicionado arquivo bin

--- buscar-cursos.php
+++ buscar-cursos.php
@@ -1,3 +1,4 @@
+#!/usr/bin/env php
 <?php
 
 require 'vendor/autoload.php';
@@ -12,6 +13,6 @@ $crawler = new Crawler();
 $buscador = new Buscador($client, $crawler);
 $cursos = $buscador->buscar('cursos-online-programacao/php');
 
-foreach ($cursos as $curso){
+foreach ($cursos as $curso) {
     echo $curso . PHP_EOL;
 }
